e855ef7729f8a7111f6f1bc92f37d9680b2fe359
Siddharth Bhat <siddu.druid@gmail.com>
2017-06-28 11:16:20 +0200

    print out IR as well

--- src/Main.hs
+++ src/Main.hs
@@ -19,5 +19,9 @@ main = do
      input <- readFile "programs/input.c"
      case parseProgram input of
         Left err -> putStrLn err
-        Right val -> putStrLn . L.unpack . prettyToText . pretty $  val
+        Right program -> do
+            putStrLn "*** Program:"
+            putStrLn . L.unpack . prettyToText . pretty $  program
+            putStrLn "*** IR:"
+            putStrLn . L.unpack . prettyToText . pretty $  programToIR program
 
