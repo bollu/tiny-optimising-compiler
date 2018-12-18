11a86c8af50d5d3d98a016e1c8e805765b111b64
Siddharth Bhat <siddu.druid@gmail.com>
2018-12-18 16:39:55 +0530

    add 2 nested while loop

---
+++ programs/while-2-nest.c
@@ -0,0 +1,15 @@
+define x;
+define y;
+define z;
+
+assign x := 10;
+assign y := 1;
+
+while (x < 12) {
+    while (y < 13) {
+        assign x := x + 1;
+        assign y := y * x;
+    };
+};
+return y;
+
